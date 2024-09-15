cd27346854911dc515f48e00c14860a3fa52e654
Vanshika Panjwani <156567018+Versuscora@users.noreply.github.com>
2024-09-15 12:19:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Base_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const Base_URL = "https://v6.exchangerate-api.com/v6/e7ba50027c5da2b33c296ea8/latest";
 
 const dropdown = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -7,54 +7,52 @@ const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
 const updateRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let val = amount.value;
+    let amount = document.querySelector(".amount input");
+    let val = amount.value;
 
-  if (val === "" || val < 1) {
-    val = 1;
-    amount.value = "1";
-  }
+    if (val === "" || val < 1) {
+        val = 1;
+        amount.value = "1";
+    }
 
-  const URL = `${Base_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
-  let finalAmount = (amount.value * rate).toFixed(2);
+    const URL = `${Base_URL}/${fromCurr.value.toUpperCase()}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.conversion_rates[toCurr.value.toUpperCase()];
 
-  msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    let finalAmount = amount.value * rate;
+    msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
 };
 
 for (let select of dropdown) {
-  for (currcode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currcode;
-    newOption.value = currcode;
-
-    if (select.name == "from" && currcode == "USD") {
-      newOption.selected = "selected";
-    } else if (select.name == "to" && currcode == "INR") {
-      newOption.selected = "selected";
+    for (currcode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currcode;
+        newOption.value = currcode;
+        if (select.name == "from" && currcode == "USD") {
+            newOption.selected = "selected";
+        } else if (select.name == "to" && currcode == "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
     }
 
-    select.append(newOption);
-  }
-
-  select.addEventListener("change", (evt) => {
-    updateflag(evt.target);
-  });
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
 }
 
-const updateflag = (element) => {
-  let currcode = element.value;
-  let countrycode = countryList[currcode];
-  let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newsrc;
+const updateFlag = (element) => {
+    let currcode = element.value;
+    let countrycode = countryList[currcode];
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newsrc;
 };
 
 btn.addEventListener("click", async (evt) => {
-  evt.preventDefault();
-  updateRate();
+    evt.preventDefault();
+    updateRate();
 });
 
 window.addEventListener("load", updateRate);
